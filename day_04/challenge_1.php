3e42d380108d0610cfa8ef18debfcc47c9de4c4b
Thomas Brinkman <tj.brinkman@gmail.com>
2022-12-04 12:58:58 +0100

    Solution for Day 4 - Comments

--- day_04/challenge_1.php
+++ day_04/challenge_1.php
@@ -4,11 +4,11 @@
     require('_read_datafile.php');
 
     function a_contains_b ($a, $b) {
-        return ($a[0] <= $b[0] && $a[1] >= $b[1]);
+        return ($a[0] <= $b[0] && $a[1] >= $b[1]); // a_start <= b_start && a_end <= b_end
     }
 
     foreach($pairs as $pair) 
-        if (a_contains_b($pair[0], $pair[1]) || a_contains_b($pair[1], $pair[0])) 
+        if (a_contains_b($pair[0], $pair[1]) || a_contains_b($pair[1], $pair[0])) // a_contains_b || b_contains_a
             @$contains++;
 
     echo "Number of pairs where one assignments fully contains the other: $contains \n";
--- day_04/challenge_2.php
+++ day_04/challenge_2.php
@@ -4,7 +4,7 @@
     require('_read_datafile.php');
 
     foreach($pairs as $pair)
-        if ($pair[0][0] <= $pair[1][1] && $pair[1][0] <= $pair[0][1])
+        if ($pair[0][0] <= $pair[1][1] && $pair[1][0] <= $pair[0][1]) // a_start <= b_end && b_start <= a_end
             @$overlap++;
 
     echo "Number of pairs where one assignment overlaps the other: $overlap \n";
